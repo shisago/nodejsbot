68717effe3ffc92429ecb93a17f81ff19c41ca40
shisago <shin@isago.com>
2016-08-22 16:59:20 +0900

    connection_strings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({ appId: '36e2fbd8-9f9f-46c0-90c5-46596a3af97c', appSecret: 'hGoSmZobEpZC893xkPYOsoN' });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
